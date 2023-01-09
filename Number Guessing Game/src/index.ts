36495b500fe97f7287fd8f15f6102e192e8f1d67
Muhammad Sajid Ali Khan <SajidRajput01.and@gmail.com>
2023-01-09 23:38:53 +0500

    cli based number guessing game

--- Number Guessing Game/src/index.ts
+++ Number Guessing Game/src/index.ts
@@ -5,33 +5,39 @@ import level from "./levelSelection.js";
 import user_guess_number from "./userInput.js";
 import { thankYou } from "./endgraphic.js";
 
-const guessingValue: number = Math.floor(Math.random() * (10 - 0 + 1) + 0);
-
 async function number_guessing_game(): Promise<void> {
   let lives: number;
   await title();
   const player_level: string = await level();
   do {
-    if (player_level === "PRO"){
+    if (player_level === "PRO") {
       lives = 3;
-    }
-    else {
+    } else {
       lives = 5;
     }
+    const guessingValue: number = Math.floor(Math.random() * (10 - 0 + 1) + 0);
     do {
       console.log(chalk.bold.yellowBright(`\nNo. of lives: ${lives}`));
       var user_number: string = await user_guess_number();
-  
+
       if (Number(user_number) > guessingValue) {
         console.log(
-          chalk.redBright(`Your number ${user_number} is greater than the guess number.\n`)
+          chalk.redBright(
+            `Your number ${user_number} is greater than the guess number.\n`
+          )
         );
         --lives;
       } else if (Number(user_number) < guessingValue) {
-        console.log(chalk.redBright(`Your number ${user_number} is less than the guess number.\n`));
+        console.log(
+          chalk.redBright(
+            `Your number ${user_number} is less than the guess number.\n`
+          )
+        );
         --lives;
       } else if (Number(user_number) === guessingValue) {
-        console.log(chalk.greenBright(`Congratulation! You guessed the right number.\n`));
+        console.log(
+          chalk.greenBright(`Congratulation! You guessed the right number.\n`)
+        );
       }
     } while (Number(user_number) !== guessingValue && lives > 0);
 
