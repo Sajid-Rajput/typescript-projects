be5c79c71e93ac582b6a5667d9ebc62ddaf9efb7
Muhammad Sajid Ali Khan <SajidRajput01.and@gmail.com>
2023-02-03 20:39:20 +0500

    Quiz Management System

---
+++ Quiz System/readme.md
@@ -0,0 +1,1 @@
+# npx sajid-quiz-system
---
+++ Quiz System/src/index.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import title from "./title.js";
+import { thankYou } from "./endgraphic.js";
+import quizMasterHead from "./askQuizQuestions.js";
+
+async function quizManagementSystem(): Promise<void> {
+  console.clear();
+  await title();
+  await quizMasterHead();
+  console.log(thankYou);
+}
+quizManagementSystem();
