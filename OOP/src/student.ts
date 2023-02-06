8bf6a9ed4f87e427a75725952071614293e68150
Muhammad Sajid Ali Khan <SajidRajput01.and@gmail.com>
2023-02-06 18:02:07 +0500

    OOP

---
+++ OOP/src/student.ts
@@ -0,0 +1,18 @@
+import Person from "./person.js";
+
+export default class Student extends Person {
+  private _name: string;
+
+  constructor() {
+    super();
+    this._name = "";
+  }
+
+  set name(n: string) {
+    this._name = n;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+}
